621aeaf0a92ab7865a7357d3b486bc16aa15f6d2
dharmik8488 <118181437+dharmik8488@users.noreply.github.com>
2022-11-14 05:09:51 -0800

    Add files via upload

---
+++ dharmik3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main(){
+
+printf("*'\t*'\t*'\t*'\t*'\n");
+printf("*'\t'\t'\t'\t'\*'\n");
+printf("*'\t'\t',hello,'\t'\t*'\n");
+printf("*'\t'\t'\t'\t*'\n");
+printf("*'\t*'\t*'\t*'\t*'\n");
+}
